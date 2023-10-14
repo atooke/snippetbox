ffb502fcd3cf542f55a27e42f8dd053230d31ba0
Andy Tooke <andy.tooke@mckesson.com>
2023-10-14 11:23:10 -0400

    add home html template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -11,8 +13,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from Snippetbox"))
+	//read template file and if error return 500 error to user.
+	//note - file path that you pass to the template.ParseFiles() function must either be relative to your current working directory, or an absolute path
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // 500
+		return
+	}
+	//use Execute() on template to set to write content as response body, last parameter is dynamic data to pass to template.  nil for now
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // 500
+	}
+
 }
+
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
