546727561e02fbde4eaba4dd48577f7ab817ad65
Andy Tooke <andy.tooke@mckesson.com>
2023-10-19 16:09:45 -0400

    add centralized error handling helper

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,13 @@
+/*
+The pattern that we’re using to inject dependencies won’t work if your handlers are spread across multiple packages. In that case,
+an alternative approach is to create a standalone config package which exports an Application struct, and have your handler functions
+close over this to form a closure. See -> https://gist.github.com/alexedwards/5cd712192b4831058b21
+*/
 package main
 
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -23,15 +27,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // 500
+		app.serverError(w, r, err)
 		return
 	}
 	//use Execute() on template to set to write content as response body, last parameter is dynamic data to pass to template.  nil for now
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // 500
+		app.serverError(w, r, err)
 	}
 
 }
@@ -39,16 +41,16 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper.
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
+
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper. return
 	}
 	w.Write([]byte("Create a new snippet..."))
 }
